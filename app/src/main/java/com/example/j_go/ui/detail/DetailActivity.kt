2b051d8b235a0a96091c670932384a2cedd6e527
Royanrp045 <royanristuprayoga@gmail.com>
2024-12-10 22:25:19 +0700

    test

--- app/src/main/java/com/example/j_go/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/j_go/ui/detail/DetailActivity.kt
@@ -1,11 +1,6 @@
 package com.example.j_go.ui.detail
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.j_go.R
 
 class DetailActivity : AppCompatActivity() {
 }
