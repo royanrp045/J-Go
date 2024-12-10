11f93d7ae03ae2fd02c09956bd69bd708f9d4db2
Royanrp045 <royanristuprayoga@gmail.com>
2024-12-10 22:21:24 +0700

    test

--- app/src/main/java/com/example/j_go/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/j_go/ui/detail/DetailActivity.kt
@@ -8,14 +8,4 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.j_go.R
 
 class DetailActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
 }
